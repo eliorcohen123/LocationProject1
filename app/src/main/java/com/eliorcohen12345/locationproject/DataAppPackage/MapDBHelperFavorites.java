6d474991a1a3600fe78d2ade9dd3f6e914617be8
EliorCohen <elior123456123123@gmail.com>
2020-09-09 15:25:01 +0300

    update

--- app/src/main/java/com/eliorcohen12345/locationproject/DataAppPackage/MapDBHelperFavorites.java
+++ app/src/main/java/com/eliorcohen12345/locationproject/DataAppPackage/MapDBHelperFavorites.java
@@ -65,7 +65,7 @@ public class MapDBHelperFavorites extends SQLiteOpenHelper {
         ContentValues contentValues = new ContentValues();
 
         Cursor cursor1;
-        cursor1 = db.rawQuery("SELECT * FROM " + MAP_TABLE_NAME + " WHERE " + MAP_LAT + "=?" + " OR " + MAP_LNG + "=?" + " OR " + MAP_NAME + "=?", new String[]{String.valueOf(lat), String.valueOf(lng), String.valueOf(name)});
+        cursor1 = db.rawQuery("SELECT * FROM " + MAP_TABLE_NAME + " WHERE " + MAP_LAT + "=?" + " AND " + MAP_LNG + "=?" + " OR " + MAP_NAME + "=?", new String[]{String.valueOf(lat), String.valueOf(lng), String.valueOf(name)});
         if (cursor1.getCount() > 0) {
             Toast.makeText(ctx, "Current place already exist in your favorites", Toast.LENGTH_LONG).show();
         } else {
